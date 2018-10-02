d3ea150c05937ba2c462d19df808ab5edc714e5f
Sean Hinchee <henesy@users.noreply.github.com>
2018-10-01 20:16:03 -0500

    ???

---
+++ smiley.c
@@ -0,0 +1,21 @@
+#include <u.h>
+#include <libc.h>
+/* Written by Rob Pike, this is just a copy for exemplification */
+typedef int ☺☹☻;
+typedef void ☹☺☻;
+enum
+{
+	☹☻☺ = sizeof(☺☹☻),
+	☻☺☹ = 0,
+};
+
+☹☺☻
+main(☹☺☻)
+{
+	☺☹☻ ☺☻☹;
+
+	for(☺☻☹=☻☺☹; ☺☻☹<☹☻☺; ☺☻☹++)
+		print("☺☻☹ = %d\n", ☺☻☹);
+		exits(☻☺☹);
+}
+
